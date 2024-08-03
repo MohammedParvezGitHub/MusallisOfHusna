990433627f5921657c71182b66244e72e7ccacb7
Mohammed Parvez <85874865+MohammedParvezGitHub@users.noreply.github.com>
2024-08-03 18:08:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Initialize Supabase client using the global Supabase object
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+
+// Replace with your Supabase URL and public API key
+const supabaseUrl = 'https://oqvxnlknzysijtzhbiyh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xdnhubGtuenlzaWp0emhiaXloIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMjY3MDMyNSwiZXhwIjoyMDM4MjQ2MzI1fQ.jIyioDs9ZgnLY4xs7rl0mFMU3Icppl5MzFf_87mzluU';
+const supabase = createClient(supabaseUrl, supabaseKey);
 const supabaseUrl = 'https://oqvxnlknzysijtzhbiyh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xdnhubGtuenlzaWp0emhiaXloIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMjY3MDMyNSwiZXhwIjoyMDM4MjQ2MzI1fQ.jIyioDs9ZgnLY4xs7rl0mFMU3Icppl5MzFf_87mzluU';
 const supabase = Supabase.createClient(supabaseUrl, supabaseKey);
