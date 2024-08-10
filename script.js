cd576c1c5d77b44fc9abf4cb72ed160515e7c647
Mohammed Parvez <85874865+MohammedParvezGitHub@users.noreply.github.com>
2024-08-10 08:43:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ async function addRecord() {
 
 // Edit a record
 async function editRecord(id) {
-    const {data,error } = await supabase
+    const {data, error } = await supabase
         .from('musalli')
         .select('*, home(home_name)')
         .eq('id', id)
